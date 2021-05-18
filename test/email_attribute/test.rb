6bf2529d938fcfd12e7195da09f3fd0629415e60
Cyril Kato <contact@cyril.email>
2021-05-18 22:24:55 +0200

    chore(gem): bundle update

--- test/email_attribute/test.rb
+++ test/email_attribute/test.rb
@@ -17,12 +17,12 @@ actual = begin
   EmailAttribute.new(required: true).call(nil)
 end
 
-raise if actual.valid? != false
-raise if actual.invalid? != true
-raise if actual.to_s != "email_attribute"
-raise if actual.value != nil
 raise if actual.error_message != ["attribute.email_attribute.errors.required", {:name=>["attribute.email_attribute.name"], :expected=>nil}]
+raise if actual.invalid? != true
 raise if actual.medium != :body
+raise if actual.to_s != "email_attribute"
+raise if actual.valid? != false
+raise if actual.value != nil
 
 # ------------------------------------------------------------------------------
 
@@ -30,12 +30,12 @@ actual = begin
   EmailAttribute.new(required: false).call(nil)
 end
 
-raise if actual.valid? != true
-raise if actual.invalid? != false
-raise if actual.to_s != "email_attribute"
-raise if actual.value != nil
 raise if actual.error_message != nil
+raise if actual.invalid? != false
 raise if actual.medium != :body
+raise if actual.to_s != "email_attribute"
+raise if actual.valid? != true
+raise if actual.value != nil
 
 # ------------------------------------------------------------------------------
 
@@ -43,12 +43,12 @@ actual = begin
   EmailAttribute.new(required: true).call(4)
 end
 
-raise if actual.valid? != false
-raise if actual.invalid? != true
-raise if actual.to_s != "email_attribute"
-raise if actual.value != 4
 raise if actual.error_message != ["attribute.email_attribute.errors.type", {:name=>["attribute.email_attribute.name"], :expected=>["expected_value.string.type"]}]
+raise if actual.invalid? != true
 raise if actual.medium != :body
+raise if actual.to_s != "email_attribute"
+raise if actual.valid? != false
+raise if actual.value != 4
 
 # ------------------------------------------------------------------------------
 
@@ -56,12 +56,12 @@ actual = begin
   EmailAttribute.new(required: false).call(4)
 end
 
-raise if actual.valid? != false
-raise if actual.invalid? != true
-raise if actual.to_s != "email_attribute"
-raise if actual.value != 4
 raise if actual.error_message != ["attribute.email_attribute.errors.type", {:name=>["attribute.email_attribute.name"], :expected=>["expected_value.string.type"]}]
+raise if actual.invalid? != true
 raise if actual.medium != :body
+raise if actual.to_s != "email_attribute"
+raise if actual.valid? != false
+raise if actual.value != 4
 
 # ------------------------------------------------------------------------------
 
@@ -69,12 +69,12 @@ actual = begin
   EmailAttribute.new(required: true).call('boom')
 end
 
-raise if actual.valid? != false
-raise if actual.invalid? != true
-raise if actual.to_s != "email_attribute"
-raise if actual.value != "boom"
 raise if actual.error_message != ["attribute.email_attribute.errors.pattern", {:name=>["attribute.email_attribute.name"], :expected=>/\A[^@]+@[^@]+$\z/i}]
+raise if actual.invalid? != true
 raise if actual.medium != :body
+raise if actual.to_s != "email_attribute"
+raise if actual.valid? != false
+raise if actual.value != "boom"
 
 # ------------------------------------------------------------------------------
 
@@ -82,12 +82,12 @@ actual = begin
   EmailAttribute.new(required: false).call('boom')
 end
 
-raise if actual.valid? != false
-raise if actual.invalid? != true
-raise if actual.to_s != "email_attribute"
-raise if actual.value != "boom"
 raise if actual.error_message != ["attribute.email_attribute.errors.pattern", {:name=>["attribute.email_attribute.name"], :expected=>/\A[^@]+@[^@]+$\z/i}]
+raise if actual.invalid? != true
 raise if actual.medium != :body
+raise if actual.to_s != "email_attribute"
+raise if actual.valid? != false
+raise if actual.value != "boom"
 
 # ------------------------------------------------------------------------------
 
@@ -95,12 +95,12 @@ actual = begin
   EmailAttribute.new(required: true).call('bob@gmail.com')
 end
 
-raise if actual.valid? != true
-raise if actual.invalid? != false
-raise if actual.to_s != "email_attribute"
-raise if actual.value != "bob@gmail.com"
 raise if actual.error_message != nil
+raise if actual.invalid? != false
 raise if actual.medium != :body
+raise if actual.to_s != "email_attribute"
+raise if actual.valid? != true
+raise if actual.value != "bob@gmail.com"
 
 # ------------------------------------------------------------------------------
 
@@ -108,9 +108,9 @@ actual = begin
   EmailAttribute.new(required: false).call('bob@gmail.com')
 end
 
-raise if actual.valid? != true
-raise if actual.invalid? != false
-raise if actual.to_s != "email_attribute"
-raise if actual.value != "bob@gmail.com"
 raise if actual.error_message != nil
+raise if actual.invalid? != false
 raise if actual.medium != :body
+raise if actual.to_s != "email_attribute"
+raise if actual.valid? != true
+raise if actual.value != "bob@gmail.com"
